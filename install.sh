e2ac29ebe145ae248dbf396fa26d8d8cf826ab11
Tatsuhiro Ikebe <beike315@icloud.com>
2022-05-14 17:49:11 +0900

    Fix (#3)

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ cd create_ap
 make install
 cd -
 cp $(find `pwd` -name raspicat_hotspot.sh) $HOME/raspicat_hotspot.sh
+sudo ln -s $(find `pwd` -name raspicat_hotspot.service) /etc/systemd/system/raspicat_hotspot.service
+sudo systemctl enable raspicat_hotspot.service
+sudo systemctl start raspicat_hotspot.service
