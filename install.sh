3f92ba2d40aca08b44924008cf65ea9e8945a5b1
Tatsuhiro Ikebe <beike315@icloud.com>
2022-05-14 18:49:27 +0900

    Fix (#4)

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y dnsmasq hostapd network-manager
 git clone https://github.com/oblique/create_ap
 cd create_ap
-make install
+sudo make install
 cd -
 cp $(find `pwd` -name raspicat_hotspot.sh) $HOME/raspicat_hotspot.sh
 sudo ln -s $(find `pwd` -name raspicat_hotspot.service) /etc/systemd/system/raspicat_hotspot.service
